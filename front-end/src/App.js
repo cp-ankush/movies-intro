6e24c8d1dba070acfed0c2f7095aeec0a2dbb43a
ankush-l <ankush@licious.in>
2018-11-04 18:07:35 +0530

    basic movie api integration with react app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_MOVIE_API_URL}/discover/movie?primary_release_date.gte=2018-10-01&primary_release_date.lte=2018-10-31&api_key=${process.env.REACT_APP_MOVIE_API_KEY}`)
+      .then(res => {
+        return res.json()
+      }).then(res => {
+        console.log(res)
+        this.setState({
+          movies: res.results
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {
+            this.state.movies.map((item) => {
+              return (<Card title={item.title} description={item.overview} img={item.poster_path} />)
+            })
+          }
         </header>
       </div>
     );
---
+++ front-end/src/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Container,
+  ImageContainer,
+  Image,
+  TextContainer,
+  Title,
+  Description
+} from './styles'
+
+
+const Card = ({title, description, img}) => {
+  return (
+    <Container>
+      <ImageContainer>
+        <Image src={`${process.env.REACT_APP_IMAGE_URL}${img}`} alt="logo" />
+      </ImageContainer>
+      <TextContainer>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+      </TextContainer>
+    </Container>
+  )
+}
+
+export default Card
